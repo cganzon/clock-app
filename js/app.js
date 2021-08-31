74b89efda590baf69a259a2db058ef0997f76622
cganzon <crganzon@gmail.com>
2021-08-30 19:20:34 -0700

    Created a function that converts the unix timestamp from the world time api and displays it on the page in the 12 hour format. Created another function that displays the correct greeting depending on the hour.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,39 @@ const getRandQuote = () => {
     });
 };
 
+const getTimeData = () => {
+    $.getJSON("http://worldtimeapi.org/api/ip", data => {
+        // console.log(data);
+        let timeZone = data.abbreviation;
+        $('.timezone').text(timeZone);
+        displayTime(data.unixtime);
+    });
+};
+
+const displayTime = (unixTime) => {
+    let date = new Date(unixTime * 1000);
+    let hours = date.getHours();
+    let convertedHours = ((date.getHours() + 11) % 12 + 1);
+    let minutes = `0${date.getMinutes()}`;
+    let am_pm = (hours >= 12) ? 'pm' : 'am';
+    let convertedTime = `${convertedHours}:${minutes.substr(-2)}`;
+    $('.time-left').text(convertedTime);
+    $('.am-pm').text(am_pm);
+    displayGreeting(hours);
+};
+
+const displayGreeting = hours => {
+    if(hours >= 5 && hours < 12) {
+        $('.greeter').text('Good morning');
+    } else if(hours >= 12 && hours < 18) {
+        $('.greeter').text('Good afternoon');
+    } else {
+        $('.greeter').text('Good evening');
+    };
+};
+
 $('.refresh').click(() => getRandQuote());
 
 getRandQuote();
+getTimeData();
+setInterval(() => getTimeData(), 1000);
